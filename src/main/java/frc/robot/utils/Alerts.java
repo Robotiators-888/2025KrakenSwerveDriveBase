d234e0ffd456064f66ca0604028bcf3ac03dd902
ajassi9751 <ajassi9751@gmail.com>
2025-12-30 23:51:06 -0500

    Added elastic notifications and the elastic color module to the Alerts

--- src/main/java/frc/robot/utils/Alerts.java
+++ src/main/java/frc/robot/utils/Alerts.java
@@ -2,13 +2,17 @@ package frc.robot.utils;
 
 import java.util.ArrayList;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+
 public class Alerts {
+    // Need to publish info to Elastic/NetworkTables
     private static Alerts INSTANCE = null;
-    ArrayList<String> stop;
+    ArrayList<String> error;
     ArrayList<String> warning;
     ArrayList<String> info;
     private Alerts() {
-        stop = new ArrayList<String>();
+        error = new ArrayList<String>();
         warning = new ArrayList<String>();
         info = new ArrayList<String>();
     }
@@ -18,14 +22,42 @@ public class Alerts {
         }
         return INSTANCE;
     }
-    public void registerStop (String alert) {
-        stop.add(alert);
+    public void registerError (String alert) {
+        error.add(alert);
+        registerColor();
     }
     public void registerWarning (String alert) {
         warning.add(alert);
+        registerColor();
     }
     public void registerInfo (String alert) {
         info.add(alert);
+        registerColor();
+    }
+    private void notifyError (String alert) {
+        Elastic.Notification notification = new Elastic.Notification(Elastic.Notification.NotificationLevel.ERROR, "Error!", alert);
+    }
+    private void notifyWarning (String alert) {
+        Elastic.Notification notification = new Elastic.Notification(Elastic.Notification.NotificationLevel.WARNING, "Error!", alert);
+    }
+    private void notifyInfo (String alert) {
+        Elastic.Notification notification = new Elastic.Notification(Elastic.Notification.NotificationLevel.INFO, "Error!", alert);
+    }
+    // Sets the single color elastic object to the highes severity level that the robot has (check engine light)
+    private void registerColor () {
+        Color exampleColor;
+        if (!error.isEmpty()) {
+            // Todo change colors, they will be hardcoded because thats a great idea :)
+            exampleColor = new Color(68, 238, 255); // Red
+        }
+        else if (!warning.isEmpty()) {
+            exampleColor = new Color(68, 238, 255); // Yellow
+        }
+        else {
+            exampleColor = new Color(68, 238, 255); // Green
+        }
+        // Not sure why this is SmartDahsboard if this is elastic
+        SmartDashboard.putString("Example Color", exampleColor.toHexString());        
     }
     public void triggerStop () {} // Need to implement this
 }
