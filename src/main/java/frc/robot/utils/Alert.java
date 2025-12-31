f50614cba71c11e8b9fbf74821f28da8dce4345e
ajassi9751 <ajassi9751@gmail.com>
2025-12-31 00:21:26 -0500

    Used method chaining for notifications to optimize garbage collector performance and fixed the titles for notifications

--- src/main/java/frc/robot/utils/Alert.java
+++ src/main/java/frc/robot/utils/Alert.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.util.Color;
 public class Alert {
   // Need to publish info to Elastic/NetworkTables
   private static Alert INSTANCE = null;
+  private static Elastic.Notification notification = new Elastic.Notification();
   ArrayList<String> error;
   ArrayList<String> warning;
   ArrayList<String> info;
@@ -35,16 +36,25 @@ public class Alert {
     registerColor();
   }
   private void notifyError (String alert) {
-    Elastic.Notification notification = new Elastic.Notification(Elastic.Notification.NotificationLevel.ERROR, "Error!", alert);
-    Elastic.sendNotification(notification);
+    Elastic.sendNotification(notification
+        .withLevel(Elastic.Notification.NotificationLevel.ERROR)
+        .withTitle("Error!")
+        .withDescription(alert)
+    );
   }
   private void notifyWarning (String alert) {
-    Elastic.Notification notification = new Elastic.Notification(Elastic.Notification.NotificationLevel.WARNING, "Error!", alert);
-    Elastic.sendNotification(notification);
+    Elastic.sendNotification(notification
+        .withLevel(Elastic.Notification.NotificationLevel.WARNING)
+        .withTitle("Warning:")
+        .withDescription(alert)
+    );
   }
   private void notifyInfo (String alert) {
-    Elastic.Notification notification = new Elastic.Notification(Elastic.Notification.NotificationLevel.INFO, "Error!", alert);
-    Elastic.sendNotification(notification);
+    Elastic.sendNotification(notification
+        .withLevel(Elastic.Notification.NotificationLevel.INFO)
+        .withTitle("Info")
+        .withDescription(alert)
+    );
   }
   // Sets the single color elastic object to the highes severity level that the robot has (check engine light)
   private void registerColor () {
